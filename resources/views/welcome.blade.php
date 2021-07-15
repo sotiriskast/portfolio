3872a6b8bb19dd24547930b561aaff9afb831f63
cersaAtrova <soteris100@gmail.com>
2021-07-15 15:04:49 +0300

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,13 @@
 
 @section('content')
 
+    @include('pages.about')
+    @include('pages.facts')
+    @include('pages.skills')
+    @include('pages.resume')
+    @include('pages.portfolio')
+    @include('pages.services')
+
+    @livewire('contact-us')
 
 @endsection
